80bdccd2ece5c3c0fbfc08845b09d666c835189e
Vasudha Jha <reachvasudha27@gmail.com>
2022-12-19 14:57:06 -0800

    FIX: internal imports issue

--- brightest_path_lib/__init__.py
+++ brightest_path_lib/__init__.py
@@ -1,4 +1,5 @@
-from .algorithm import AStarSearch
-from .input import CostFunction, HeuristicFunction, SearchFunction
+from brightest_path_lib.algorithm import AStarSearch
+from brightest_path_lib.input import CostFunction, HeuristicFunction, SearchFunction
+
 def example_function():
     return 1 + 1
