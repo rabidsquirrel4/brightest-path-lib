72bf935b6049a252b3af787a1b9275373831ea30
Vasudha Jha <reachvasudha27@gmail.com>
2022-12-19 21:54:36 -0800

    fix ci

--- setup.py
+++ setup.py
@@ -1,22 +1,10 @@
 from setuptools import setup, find_packages
-import os
 
 VERSION = "0.1"
-
-
-def get_long_description():
-    with open(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
-        encoding="utf8",
-    ) as fp:
-        return fp.read()
-
 
 setup(
     name="brightest-path-lib",
     description="A library of path-finding algorithms to find the brightest path between two points.",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
     author="Vasudha Jha",
     url="https://github.com/mapmanager/brightest-path-lib",
     project_urls={
